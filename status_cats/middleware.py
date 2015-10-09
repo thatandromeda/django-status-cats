f1890d935aab4d27698eb513123c569e03a895c6
Andromeda Yelton <andromeda.yelton@gmail.com>
2015-10-09 16:42:01 -0400

    Refactor and obey header-only setting

--- status_cats/middleware.py
+++ status_cats/middleware.py
@@ -5,24 +5,14 @@ status codes.
 
 from django.shortcuts import render
 
-from status_cats.constants import CAT_URLS, CAT_TEMPLATE, BASE_TEMPLATE
+from status_cats.constants import CAT_URLS
+from status_cats.settings import CAT_TEMPLATE, BASE_TEMPLATE, HEADER_OVERRIDE_ONLY
 
 """
 TODO:
 
-* add config options:
-    * a list of status codes to modify only headers - should be named constants
-      and should contain 200 by default
-    * CAT_TEMPLATE, BASE_TEMPLATE should get from settings
-      but have a default (content-block-name looks hard)
-    * consider a few options for your settings API:
-        https://github.com/tomchristie/django-rest-framework/blob/master/rest_framework/settings.py
-        https://stackoverflow.com/questions/16953293/what-is-the-equivalent-to-django-conf-global-settings-for-third-party-apps
-        https://github.com/pinax/django-stripe-payments/blob/master/payments/settings.py
-            ^^^ probably this last
 * add README, including tested versions/dependencies and cat template override instructions
 * add to pypi
-* add proper credit to https://www.flickr.com/photos/girliemac/sets/72157628409467125/
 * consider setting to handle only RFC compliant ones?
 * deal with RFC-compliant codes not present in photo set
 
@@ -36,19 +26,22 @@ DONE:
 * write views that return each status code for testing
 * fill in _render_with_header
 * use _render_with_header for non-template get() <- actually this is on the webserver side
+* CAT_TEMPLATE, BASE_TEMPLATE should get from settings but have a default (content-block-name looks hard)
+* add proper credit to https://www.flickr.com/photos/girliemac/sets/72157628409467125/
+* add config options:
+    * a list of status codes to modify only headers - should contain 200 by default
+
 
 
 """
 
 class StatusCatMiddleware(object):
 
-    def _render_with_cat(self, request, response):
+    def _render_with_cat(self, request, status_code, cat_url):
         """
         This renders the specified CAT_TEMPLATE with context of cat_url
         (image URL for the relevant HTTP status cat) and status_code.
         """
-        status_code = response.status_code
-        cat_url = CAT_URLS[int(status_code)]
         return render(request, CAT_TEMPLATE,
             {'cat_url': cat_url,
              'base_template': BASE_TEMPLATE,
@@ -56,7 +49,7 @@ class StatusCatMiddleware(object):
              status=status_code)
 
 
-    def _add_header(self, response):
+    def _add_header(self, response, cat_url):
         """
         This adds the image URL for the relevant status code to the
         HTTP headers, but does not otherwise change the HTTP response.
@@ -66,22 +59,26 @@ class StatusCatMiddleware(object):
         want to render their app's template and not cats. (There's no accounting
         for tastes.)
         """
-        status_code = response.status_code
-        cat_url = CAT_URLS[int(status_code)]
         response['X-Status-Cat'] = cat_url
         return response
 
 
     def _inner_process(self, request, response):
-        # It's important to render the response first and add the header
-        # second, as the render process will overwrite the headers.
-        response = self._render_with_cat(request, response)
-        return self._add_header(response)
+        status_code = int(response.status_code)
+        cat_url = CAT_URLS[status_code]
+
+        if status_code in HEADER_OVERRIDE_ONLY:
+            return self._add_header(response, cat_url)
+        else:
+            # It's important to render the response first and add the header
+            # second, as the render process will overwrite the headers.
+            response = self._render_with_cat(request, status_code, cat_url)
+            return self._add_header(response, cat_url)
 
 
     def process_template_response(self, request, response):
-        self._inner_process(request, response)
+        return self._inner_process(request, response)
 
 
     def process_response(self, request, response):
-        self._inner_process(request, response)
+        return self._inner_process(request, response)
